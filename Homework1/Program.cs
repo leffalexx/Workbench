fbfdca2819fc006a2626557c0a3f5e54a8ca6256
aleksei_namestnikov <leffalexx@gmail.com>
2022-06-07 22:45:53 +0300

    Second task done

--- Homework1/Program.cs
+++ Homework1/Program.cs
@@ -14,5 +14,39 @@ else
 {
     Console.WriteLine("max = " + num2);
 }
-*/
 
+
+int num1, num2, num3, max;
+
+Console.Write("Input first number ");
+num1 = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Input second number ");
+num2 = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Input third number ");
+num3 = Convert.ToInt32(Console.ReadLine());
+
+max = num1;
+
+if (max <= num2)
+{
+    max = num2;
+}
+else
+{
+    max = num1;
+}
+if (max <=num3)
+{
+    max = num3;
+}
+else
+{
+    max = num1;
+}
+
+Console.WriteLine("max = " + max);
+
+*/
+    
