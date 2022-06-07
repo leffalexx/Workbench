ac79f4532fd959136ce377e7eb947fe874c2b287
aleksei_namestnikov <leffalexx@gmail.com>
2022-06-07 22:12:49 +0300

    first task done

--- Homework1/Program.cs
+++ Homework1/Program.cs
@@ -1,2 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/* int num1, num2;
+
+Console.Write("Input first number ");
+num1 = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Input second number ");
+num2 = Convert.ToInt32(Console.ReadLine());
+
+if (num1 > num2)
+{
+    Console.WriteLine("max = " + num1);
+}
+else
+{
+    Console.WriteLine("max = " + num2);
+}
+*/
+
