718ade7979dde4ff678a2d416f2743bcfc8f978a
aleksei_namestnikov <leffalexx@gmail.com>
2022-06-14 21:07:02 +0300

    First task done

--- Homework2/Program.cs
+++ Homework2/Program.cs
@@ -1,2 +1,19 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+
+int MiddleDigit(int num)
+{
+    int hund = num / 100;
+    int dig = num % 10;
+    int result = (num - hund*100 - dig)/10;
+    return result;
+}
+
+int num;
+
+Console.Write("Input 3 digit number ");
+num = Convert.ToInt32(Console.ReadLine());
+
+int result = MiddleDigit(num);
+
+Console.WriteLine(result);
+
