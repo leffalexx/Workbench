e89c69f26db1b1545232537bdc3a64f9b555820f
aleksei_namestnikov <leffalexx@gmail.com>
2022-06-14 22:42:56 +0300

    2 в процессе

--- Homework2/Program.cs
+++ Homework2/Program.cs
@@ -1,5 +1,5 @@
 ﻿
-
+/*
 int MiddleDigit(int num)
 {
     int hund = num / 100;
@@ -16,4 +16,28 @@ num = Convert.ToInt32(Console.ReadLine());
 int result = MiddleDigit(num);
 
 Console.WriteLine(result);
+*/
 
+
+int ThirdDigit(int num)
+        {
+            if (num / 100 < 0)
+                Console.WriteLine("Третьей цифры нет");
+            else
+            {
+                while (num / 100 > 1)
+                {
+                    num = num / 10;
+                }
+            }
+            num = num % 10;
+        }
+
+        int num;
+        
+        Console.Write("Input any number ");
+        num = Convert.ToInt32(Console.ReadLine());
+
+        int result = ThirdDigit(num);
+
+        Console.WriteLine(result);
