2787b3c5d60e92ab32c95da9c2f951d15a3d0942
aleksei_namestnikov <leffalexx@gmail.com>
2022-06-16 10:38:50 +0300

    first task done

--- Seminar4/Program.cs
+++ Seminar4/Program.cs
@@ -1,2 +1,20 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+// Найти сумму цифр в числе
+/*
+int FindSumOfDigits(int num)
+{
+    int sum = 0;
+
+    while(num > 0)
+    {
+        sum = sum + num % 10; //sum += num % 10;
+        num = num / 10;       // num /= 10;
+    }
+    return sum;
+}
+
+int n = 234;
+int result = FindSumOfDigits(n);
+
+Console.WriteLine($"Сумма цифр в числе {n} равна {result}");
+*/
