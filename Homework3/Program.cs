e179576b171c46e458531a7362cd50950693b8d9
aleksei_namestnikov <leffalexx@gmail.com>
2022-06-19 18:53:56 +0300

    done

--- Homework3/Program.cs
+++ Homework3/Program.cs
@@ -4,7 +4,7 @@
 bool IfPalindrome (int num)
 {
     int first = num / 10000;
-    int second = (num / 10000) - (num / 1000);
+    int second =  num / 1000 - (num / 10000) * 10;
     int fourth = (num % 100) / 10;
     int fifth = num % 10;
     if ((first ==  fifth) && (second == fourth)) return true;
@@ -20,13 +20,17 @@ bool result = IfPalindrome(num);
 Console.WriteLine(result);
 */
 
+
+//Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+
+/*
 void Qube(int num)
 {
-    int current = 1;
-    while(current <= num)
+       // while (current <= num)
+   for (int x = 1; x <= num; x++)
     {
-        Console.Write(Math.Pow(current,3) + " ");
-        current++;
+        Console.Write(Math.Pow(x,3) + " ");
+        //current++;
     }
 }
 
@@ -34,3 +38,37 @@ Console.WriteLine("Введите число");
 int num = Convert.ToInt32(Console.ReadLine());
 
 Qube(num);
+*/
+
+
+
+//Напишите программу, которая принимает на вход координаты двух точек и находит расстояние между ними в 3D пространстве
+
+
+double ThreeDLength (double xA, double yA, double zA, double xB, double yB, double zB)
+{
+   return Math.Sqrt(Math.Pow((xB-xA),2) + Math.Pow((yB - yA),2) + (Math.Pow((zB-zA),2))); 
+}
+
+Console.WriteLine("Введите координату xA");
+double xA = Convert.ToDouble(Console.ReadLine());
+
+Console.WriteLine("Введите координату yA");
+double yA = Convert.ToDouble(Console.ReadLine());
+
+Console.WriteLine("Введите координату zA");
+double zA = Convert.ToDouble(Console.ReadLine());
+
+Console.WriteLine("Введите координату xB");
+double xB = Convert.ToDouble(Console.ReadLine());
+
+Console.WriteLine("Введите координату yB");
+double yB = Convert.ToDouble(Console.ReadLine());
+
+Console.WriteLine("Введите координату zB");
+double zB = Convert.ToDouble(Console.ReadLine());
+
+double result;
+result = ThreeDLength(xA, yA, zA, xB, yB, zB);
+
+Console.WriteLine(result);
