795c9726a8d0b6686ff09e195ac9e95ccad49f06
aleksei_namestnikov <leffalexx@gmail.com>
2022-06-20 19:14:01 +0300

    done

--- Homework4/Program.cs
+++ Homework4/Program.cs
@@ -1,5 +1,6 @@
 ﻿//Напишите программу, которая принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
 
+/*
 int Exponent(int a, int b)
 {
    // int power = (int)Math.Pow(a,b);
@@ -24,4 +25,20 @@ if(b > 0)
 else
 {
 Console.WriteLine("Попробуйте еще раз");
+}
+*/
+
+// Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+
+int size = 8;
+int[] array = new int[size];
+
+for(int i = 0; i < size; i++)
+{
+    Console.Write("Введите целое число ");
+    array[i] = Convert.ToInt32(Console.ReadLine());
+}
+for (int i = 0; i < size; i++)
+{
+    Console.Write(array[i] + " ");
 }
