2badaa742e3911a2702da9dd6414446c79c01e83
aleksei_namestnikov <leffalexx@gmail.com>
2022-06-20 18:24:36 +0300

    first done

--- Homework4/Program.cs
+++ Homework4/Program.cs
@@ -1,2 +1,27 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//Напишите программу, которая принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+
+int Exponent(int a, int b)
+{
+   // int power = (int)Math.Pow(a,b);
+   int power = a;
+   for (int count = 1; count < b; count++)
+   {
+           power = power * a;
+   }
+    return power;
+}
+
+Console.WriteLine("Введите целое число");
+int a = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите натуральное число");
+int b = Convert.ToInt32(Console.ReadLine());
+
+if(b > 0)
+{
+  Console.WriteLine("Результат равен " + Exponent(a,b));
+}
+else
+{
+Console.WriteLine("Попробуйте еще раз");
+}
