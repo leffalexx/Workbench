3c10967413e2e1fbab21212fa6d4f1f8eda3f120
aleksei_namestnikov <leffalexx@gmail.com>
2022-06-23 10:48:09 +0300

    2 done

--- Seminar6/Program.cs
+++ Seminar6/Program.cs
@@ -1,2 +1,43 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача на разворот массива
+
+/*
+int[] ReverseArray(int[] array)
+{
+    int temp;
+    int j = array.Length -1;
+
+    for(int i = 0; i < array.Length / 2; i++, j--)
+    {
+        temp = array[i];
+        array[i] = array[j];
+        array[j] = temp;
+    }
+    return array;
+}
+
+int[] myArray = {2, 4, 6, 8, 10};
+
+myArray = ReverseArray(myArray);
+
+for(int i = 0; i < myArray.Length; i++)
+{
+    Console.Write(myArray[i] + " ");
+}
+*/
+
+
+//Напишите программу, которая принимает на вход три числа и проверяет, может ли существовать треугольник с сторонами такой длины.
+
+/*
+bool TrianglePossibility(int numA, int numB, int numC)
+{
+   if ((numA + numB > numC) && (numB + numC > numA) && (numC + numA > numB)) return true;
+   return false; 
+}
+
+Console.WriteLine(TrianglePossibility (3, 3, 3));
+*/
+
+
+//Не используя рекурсию, выведите первые N чисел Фибоначчи. Первые два числа Фибоначчи: 0 и 1.
+
