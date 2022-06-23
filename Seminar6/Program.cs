b5a26f91a4ef6efe492df306de9d972afcec80d1
aleksei_namestnikov <leffalexx@gmail.com>
2022-06-23 15:59:39 +0300

    done

--- Seminar6/Program.cs
+++ Seminar6/Program.cs
@@ -41,3 +41,48 @@ Console.WriteLine(TrianglePossibility (3, 3, 3));
 
 //Не используя рекурсию, выведите первые N чисел Фибоначчи. Первые два числа Фибоначчи: 0 и 1.
 
+/*
+
+int ArrayFibonacci(int num)
+{
+    int[] array = new int[num];
+    array[0] = 0;
+    array[1] = 1;
+    for(int i = 2; i < num; i++)
+    {
+        array[i] = array[i-2] + array[i-1];
+    }
+    return array;
+}
+
+string ArrayToString(int[] array)
+{
+    string[] arrayString = new String[array.Length];
+    string arrayToString = "";
+    for(int i = 0; i < array.Length; i++)
+    {
+        arrayString[i] = Convert.ToString(array [i]);
+    }
+    ArrayToString = string.Join(", ", arrayString);
+    return ArrayToString;
+}
+
+Console.WriteLine(ArrayToString(ArrayFibonacci(4)));
+
+*/
+
+
+//Напишите программу, которая будет преобразовывать десятичное число в двоичное.
+
+string DecimalToDoble(int num)
+{
+    string result = string.Empty;
+    while (num > 0)
+    {
+        result = num % 2 + result;
+        num /= 2; // num = num / 2
+    }
+    return result;
+}
+
+Console.WriteLine(DecimalToDoble(8));
