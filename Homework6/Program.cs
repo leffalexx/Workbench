3c8afec34cd84b89c62693aab70fc66a7859c87b
aleksei_namestnikov <leffalexx@gmail.com>
2022-06-29 17:07:44 +0300

    1st done

--- Homework6/Program.cs
+++ Homework6/Program.cs
@@ -1,2 +1,35 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+/*
+Console.Write("Введите количество чисел для проверки ");
+int arraySize = Convert.ToInt32(Console.ReadLine());
+
+int[] newArray = new int[arraySize];
+
+for (int i = 0; i < newArray.Length; i++)
+{
+    Console.WriteLine("Введите число номер " + (i+1));
+    newArray[i] = Convert.ToInt32(Console.ReadLine());
+}
+
+Console.WriteLine();
+
+for (int i = 0; i < newArray.Length; i++)
+{
+    Console.Write(newArray[i] + " ");
+}
+
+Console.WriteLine();
+
+int AmountOfPositive(int[] newArray)
+{
+  int sum = 0;
+  for(int i = 0; i < newArray.Length; i++)
+  {
+    if(newArray[i] > 0) sum++;
+  }
+  return sum;
+}
+
+Console.WriteLine("Количество чисел больше нуля " + AmountOfPositive(newArray));
+*/
+
