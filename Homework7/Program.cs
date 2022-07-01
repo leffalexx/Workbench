29da62c34b8845499d9a47c1af9e2349f3d61eb6
aleksei_namestnikov <leffalexx@gmail.com>
2022-07-01 17:48:37 +0300

    first kinda done

--- Homework7/Program.cs
+++ Homework7/Program.cs
@@ -1,2 +1,22 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//Задайте двумерный массив размером m×n, заполненный случайными вещественными числами.
+
+double[,] CreateRandomTwoDemArray(int m, int n)
+{
+    double[,] newMatrix = new double[m, n];
+    
+    for (int i = 0; i < m; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            newMatrix[i,j] = Math.Round(new Random().NextDouble()*10, 1);
+            Console.Write(newMatrix[i,j] + " ");
+        }
+        Console.WriteLine();
+    }
+
+    return newMatrix;
+
+}
+
+double[,] array = CreateRandomTwoDemArray(3, 4);
+
