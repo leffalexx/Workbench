cb4dae81f9ea9f390a67a8ce72be9c6c7219534a
aleksei_namestnikov <leffalexx@gmail.com>
2022-07-05 21:41:36 +0300

    Blank ready

--- Testing/Program.cs
+++ Testing/Program.cs
@@ -1,2 +1,56 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿public class Transport
+{
+    public string Name;
+    public string TransportType = "Transport";
+    public double Speed;
+public virtual void PrintDrivingInfo(double distance)
+{
+    Console.WriteLine(TransportType + " will travel " + distance + " km");
+}
+
+}
+public class Car : Transport
+{
+    public Car(double consumption)
+    {
+        TransportType = "Car";
+        LitersPer100Km = consumption;
+    }
+    public Car(): this(7)
+    {
+       
+    }
+    public double LitersPer100Km;
+    public override void PrintDrivingInfo(double distance)
+    {
+        base.PrintDrivingInfo(distance);
+        Console.WriteLine("Skoda ftw " + LitersPer100Km);
+    }
+}
+public class Bicycle : Transport
+{
+    public Bicycle()
+    {
+        TransportType = "Bicycle";
+    }
+}
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        for(int x = 0; x < args.Length; x++)
+        {
+            Console.WriteLine(args[x]);
+        }
+        Bicycle lehaBike = new Bicycle();
+        lehaBike.Name = "Merida";
+        lehaBike.Speed = 20;
+
+        Car lehaCar = new Car();
+        lehaCar.Name = "Skoda Octavia";
+        lehaCar.Speed = 100;
+
+        lehaBike.PrintDrivingInfo(20);
+        lehaCar.PrintDrivingInfo(20);
+    }
+}
