0bc8d96c95e16ce0d6c41bd8bb40931882d1c77f
aleksei_namestnikov <leffalexx@gmail.com>
2022-07-06 22:18:55 +0300

    done

---
+++ Homework9/Program.cs
@@ -0,0 +1,41 @@
+﻿// Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
+
+/*
+int SumNumbers (int m, int n)
+{
+    if (m == n)
+    {
+        return m;
+    }
+    else
+    {
+        return n + SumNumbers(m, n - 1);
+    }
+}
+
+Console.Write("input lesser number: ");
+int num1 = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("input bigger number: ");
+int num2 = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine(SumNumbers(num1, num2));
+*/
+
+// Напишите программу, которая будет принимать на вход число и возвращать кол-во его цифр.
+
+int QuantityOfDigits(int num)
+{
+    if (num < 10)
+    {
+        return 1;
+    }    
+    else
+    {
+        return 1 + QuantityOfDigits(num/10);
+    }   
+}
+
+Console.Write("input number: ");
+int number = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("There are " + QuantityOfDigits(number) + " digits in given number");
