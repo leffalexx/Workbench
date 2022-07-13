1fd417ed19d37e8f43c82f6631c33ce8c4cb1c31
aleksei_namestnikov <leffalexx@gmail.com>
2022-07-13 17:34:52 +0300

    оптимизировал первую и сделал вторую

--- Homework10/Program.cs
+++ Homework10/Program.cs
@@ -1,5 +1,6 @@
 ﻿// Задайте массив строк. Напишите программу, считает кол-во слов в массиве, начинающихся на гласную букву.
 
+/*
 
 string[] carManufacturers = {"Hispano-Suiza", "Alfa Romeo", "Isotta Fraschini", "Maserati", "Duesenberg", "Cord"};
 
@@ -11,21 +12,59 @@ void ShowArray(string[] array)
     Console.WriteLine();
 }
 
-int StartWithWowel(string[] array)
+int StartWithVowel(string[] array)
 {
     int count = 0;
+    string vowels = "EeYyUuIiOoAa";
     for(int i = 0; i < array.Length; i++)
+    {
+        for (int j = 0; j < vowels.Length; j++)
         {
-            if (array[i][0] == 'A' || array[i][0] == 'a' 
-             || array[i][0] == 'E' || array[i][0] == 'e'
-             || array[i][0] == 'O' || array[i][0] == 'o'
-             || array[i][0] == 'Y' || array[i][0] == 'y'
-             || array[i][0] == 'U' || array[i][0] == 'u'
-             || array[i][0] == 'I' || array[i][0] == 'i')
+            if (array[i][0] == vowels[j])
             count++;
         }
+    }
+        
     return count;
 }
 
 ShowArray(carManufacturers);
-Console.WriteLine("Названий, начинающихся на гласную букву " + StartWithWowel(carManufacturers));
+Console.WriteLine("Названий, начинающихся на гласную букву " + StartWithVowel(carManufacturers));
+
+*/
+//  Задайте массив строк. Напишите программу, которая генерирует новый массив, объединяя элементы исходного массива попарно.
+
+
+string[] randomWords = {"First", "Watermelon", "Fish", "Coal", "Streetart", "Gym", "Sea"};
+
+void ShowArray(string[] array)
+{
+    for(int i = 0; i < array.Length; i++)
+        Console.Write(array[i] + " ");
+
+    Console.WriteLine();
+}
+
+string[] CombiningPairs(string[] array)
+{
+    int size = array.Length / 2;
+    if (array.Length % 2 == 1)
+    {
+        size++;
+    }
+    string[] pairs = new string[size];
+    for (int i = 0; i < array.Length / 2; i++)
+    {
+        int j = i * 2;
+        pairs[i] = array[j] + array[j+1];
+    }
+    if (array.Length % 2 == 1)
+    {
+        pairs[size - 1] = array[array.Length - 1];
+    }
+    return pairs;
+}
+
+ShowArray(randomWords);
+Console.WriteLine();
+ShowArray(CombiningPairs(randomWords));
